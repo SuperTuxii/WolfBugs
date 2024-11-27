728cef990e58a1ee771514a82b1fe7cec36b92e2
SuperTuxii <123881249+SuperTuxii@users.noreply.github.com>
2024-11-27 21:16:45 +0100

    Only applies morphed skin (PlayerInfo) to AbstractClientPlayer which prevents the skin parts in the PlayerTabOverlay and SocialInteractionPlayerList from getting changed. Therefore, morphing only applies to the actual ingame player model.

--- src/main/java/tuxi/wolfbugs/mixininterface/MorphPlayerInfo.java
+++ src/main/java/tuxi/wolfbugs/mixininterface/MorphPlayerInfo.java
@@ -1,13 +1,10 @@
 package tuxi.wolfbugs.mixininterface;
 
-import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import net.minecraft.client.multiplayer.PlayerInfo;
-import net.minecraft.resources.ResourceLocation;
-
-import java.util.Map;
 
 public interface MorphPlayerInfo {
-    Map<MinecraftProfileTexture.Type, ResourceLocation> wolfBugs$getTextureLocations();
+    PlayerInfo wolfBugs$getMorphedPlayerInfo();
+    boolean wolfBugs$isMorphed();
     void wolfBugs$morph(PlayerInfo morphIntoPlayerInfo);
     void wolfBugs$unmorph();
 }
